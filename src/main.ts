bb8fc4a86bf19c7a72f14872970759deadacfc26
LucaCiani <lucaciani88@gmail.com>
2026-01-05 19:09:13 +0100

    snack 3 + bonus complete

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-// SNACK-1
+// 🏆 SNACK-1
 
 // Hai ricevuto un dato generico da un'API, ma non sai di che tipo sia… Il tuo compito è controllare il tipo del dato e stampare il valore in modo corretto.
 // Se è una stringa: stampala in maiuscolo
@@ -32,7 +32,7 @@ if (typeof datiGenerici === "string") {
     console.log("Tipo non supportato");
 }
 
-// SNACK-2
+// 🏆 SNACK-2
 
 // Crea un type alias Dipendente che rappresenta un lavoratore con i seguenti dati:
 
@@ -56,4 +56,46 @@ type Dipendente = {
     anniDiServizio: number[];
     readonly emailAziendale: string;
     contratto: "indeterminato" | "determinato" | "freelance";
+};
+
+// 🏆 SNACK-3
+
+// Estendiamo Dipendente per definire due ruoli specifici all'interno dell'azienda:
+
+// 1. Developer
+// livelloEsperienza → Il livello di esperienza del developer (le scelte possibili sono solo “Junior”, “Mid” o “Senior”).
+// linguaggi → Un array contenente i linguaggi di programmazione utilizzati dal developer in azienda (opzionale, perché i neo assunti non hanno ancora dei linguaggi assegnati).
+// certificazioni → Un array di stringhe contenente certificazioni tecniche ottenute dal developer (può essere vuoto).
+
+// 2. ProjectManager
+// teamSize → Il numero di persone nel team gestito dal Project Manager (può essere null se non ha ancora un team assegnato).
+// budgetGestito → Il totale del budget annuale gestito dal PM (opzionale).
+// stakeholderPrincipali → Un array di stringhe con i nomi dei principali stakeholder con cui il PM collabora (può essere vuoto).
+
+// 🎯 BONUS
+
+// Definiamo un nuovo type alias Team, che rappresenta un gruppo di lavoro all'interno dell'azienda:
+
+// nome → Nome del team (stringa).
+// progettoAttuale → Nome del progetto su cui lavora il team (può essere null se il team è in attesa di assegnazione).
+// budget → Importo numerico del budget assegnato al team (sempre presente).
+// membri → Una tuple in cui il primo elemento è sempre un Project Manager, seguito da uno o più Developers (almeno un developer obbligatorio).
+
+type Developer = Dipendente & {
+    livelloEsperienza: "Junior" | "Mid" | "Senior";
+    linguaggi?: string[];
+    certificazioni: string[];
+};
+
+type ProjectManager = Dipendente & {
+    teamSize: number | null;
+    budgetGestito?: number;
+    stakeholderPrincipali: string[];
+};
+
+type Team = {
+    nome: string;
+    progettoAttuale: string | null;
+    budget: number;
+    membri: [ProjectManager, Developer, ...Developer[]];
 };
